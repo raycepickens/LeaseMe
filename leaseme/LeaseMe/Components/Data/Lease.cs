ceb90d34a9ecc9fef0dd2f73fd7fd4f74cd7fcb5
raycepickens <113872063+raycepickens@users.noreply.github.com>
2024-04-13 17:15:00 -0400

    Add files via upload
    Commit 1, initial app framwork\pages

---
+++ leaseme/LeaseMe/Components/Data/Lease.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.Contracts;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+using System.Threading.RateLimiting;
+
+namespace LeaseMe.Components.Data
+{
+    public class Lease
+    {
+        public string Poster { get; set; }         
+        public string Contact { get; set; }        
+        public string ImgId { get; set; }          
+        public string PropertyInfo { get; set; }   
+        public double Price { get; set; }          
+        public string LeasePeriod { get; set; }
+        public string PropertyName { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Poster} is posting a property at {PropertyName} for ${Price}" +
+                $" from {LeasePeriod} \nadditional property info: {PropertyInfo}\nContact them at {Contact}";
+        }
+    }
+}
